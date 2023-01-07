571fea0742c768ffddb461ad186564fe99c02b65
Bernardo Meireles <67381633+dv-nt@users.noreply.github.com>
2023-01-07 01:16:11 +0000

    / & /auth

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const btoa = require('btoa');
+const axios = require("axios");
 
 const config = require("./config.json")
+
+const app = express();
 
 app.get('/', (req, res) => {
     res.redirect([
@@ -10,6 +14,36 @@ app.get('/', (req, res) => {
         '&response_type=code',
         `&callback_uri=${config.redirect_url}`
       ].join(''));
+});
+
+app.get('/auth', (req, res) => {
+    const code = req.query.code;
+    const cred = btoa(`${config.application_id}:${config.client_secret}`);
+
+    console.log(code);
+
+
+    const options = 
+
+    axios.post(`https://discordapp.com/api/oauth2/token`, {
+        "client_id": config.application_id,
+        "client_secret": config.client_secret,
+        "grant_type": "authorization_code",
+        "code": code
+    }, {
+        headers: {
+            'Authorization': `Basic ${cred}`,
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    }).then((resp) => {
+
+        // handle data
+
+    }, (error) => {
+
+        console.log(error);
+
+    });
 });
 
 app.listen(8080, "0.0.0.0", () => console.log('Ready'));
